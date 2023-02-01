50901754951a6b0dbe4db83c8b29dcb8dac62dec
강은빈 <kub0803@naver.com>
2023-02-02 01:16:56 +0900

    feat: circle 슬라이더

---
+++ lib/widget/circle_slider.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_start_app/modal/modal_movie.dart';
+
+class CircleSlider extends StatelessWidget {
+  final List<Movie> movies;
+
+  const CircleSlider({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(7),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('미리보기'),
+          Container(
+            height: 120,
+            child: ListView(
+              //  Axis.vertical 위 아래로 스크롤 가능 (default : vertical)
+              scrollDirection: Axis.horizontal,
+              children: makeCircleImages(movies),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/// InkWell 위젯
+/// Container와 같이 제스쳐 기능을 제공하지 않는 위젯을 래핑하여 onTap 기능 제공
+/// InkWell 위젯을 탭하면 물결 모양의 애니메이션 효과가 발생함
+List<Widget> makeCircleImages(List<Movie> movies) {
+  List<Widget> result = [];
+  for (var i = 0; i < movies.length; i++) {
+    result.add(
+      InkWell(
+        onTap: () {},
+        child: Container(
+          padding: const EdgeInsets.only(right: 10),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: CircleAvatar(
+              backgroundImage: AssetImage('images/' + movies[i].poster),
+              radius: 48,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  return result;
+}
